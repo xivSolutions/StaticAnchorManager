9b205f979e13a2559e791178f66385bea217a36f
John Atten <xivSolutions@gmail.com>
2012-10-15 22:21:54 -0500

    AnchorData - Modified AnchorName property setter to strip non-alpha chars

--- WLWSimpleAnchorManager/AnchorData.cs
+++ WLWSimpleAnchorManager/AnchorData.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace WLWSimpleAnchorManager
 {
     public class AnchorData
     {
+        private string rgxOnlyAlphaNumeric = "[^0-9a-zA-Z-]";
+        private string _anchorName;
+
+
         public string DisplayText { get; set; }
-        public string AnchorName { get; set; }
+
+        public string AnchorName 
+        { 
+            get 
+            { 
+                return _anchorName; 
+            } 
+            set
+            {
+                var rgx = new Regex(rgxOnlyAlphaNumeric);
+                _anchorName = rgx.Replace(value, "-");
+            }
+        
+        }
+
         public AnchorTypes AnchorType { get; set; }
     }
 }
