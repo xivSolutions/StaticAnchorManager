3b6722e40cbb6e057afdbaccab89f85ed58f435f
John Atten <xivsolutions@gmail.com>
2012-10-28 15:10:16 -0500

    InlineAnchorManager - Consolidate Code to consume EditorContent for core post content html operations

--- WLWSimpleAnchorManager/InLineAnchorManager.cs
+++ WLWSimpleAnchorManager/InLineAnchorManager.cs
@@ -28,23 +28,21 @@ namespace WLWSimpleAnchorManager
         private static string LINK_ICON_KEY = Properties.Resources.LINK_IMAGE_KEY;
 
         private string _editorHtml;
-        private string _selectedHtml;
-        private string _selectedText;
+        private string _selectedHtml = "";
+        private string _selectedText = "";
         private string[] _anchorsList;
-        IHTMLDocument2 _htmlDoc;
 
         private string _currentAnchorName = "";
 
         public override DialogResult CreateContent(IWin32Window dialogOwner, ref string content)
         {
-            _editorHtml = WLWPostContentHelper.ExtractHtml(dialogOwner.Handle);
-            _selectedText = WLWPostContentHelper.ExtractSelectedText(dialogOwner.Handle);
+            EditorContent currentEditor = new EditorContent(dialogOwner.Handle);
+            _editorHtml = currentEditor.EditorHtml;
             _anchorsList = WLWPostContentHelper.getAnchorNames(_editorHtml);
-            _htmlDoc = WLWPostContentHelper.getHtmlDocument(dialogOwner.Handle);
 
             try
             {
-                IHTMLElement currentElement = this.getCurrentElement(_htmlDoc);
+                IHTMLElement currentElement = currentEditor.TryGetCurrentElement();
                 _selectedHtml = currentElement.outerHTML;
                 _selectedText = currentElement.innerText;
             }
@@ -82,7 +80,6 @@ namespace WLWSimpleAnchorManager
                             break;
                     }
 
-
                     // If no text is selected in the editor, a named anchor will be inserted at the 
                     // the cursor location, but will not be bound to a specific HTML text element:
                     if (builder != null)
@@ -111,73 +108,6 @@ namespace WLWSimpleAnchorManager
                     return DialogResult.Cancel;
                 }
             }
-        }
-
-
-        IHTMLElement getCurrentElement(IHTMLDocument2 htmlDocument)
-        {
-            IHTMLSelectionObject selection = htmlDocument.selection;
-
-            // THis line will throw an exception if an Image or other non-Html
-            // item is selected in the editor. Allow the exception to propegate
-            // up the call stack for handling at the UI level. 
-            IHTMLTxtRange rng = selection.createRange() as IHTMLTxtRange;
-            IHTMLElement elmt = null;
-
-            try
-            {
-                elmt = this.getFirstValidSelectionElement(rng.parentElement());
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-
-            rng.moveToElementText(elmt);
-            rng.select();
-            return elmt;
-        }
-
-
-        IHTMLElement getFirstValidSelectionElement(IHTMLElement intialElement)
-        {
-            
-            if (Array.IndexOf(this.validSelectionElementClassNames(), intialElement.GetType().Name) >= 0)
-            {
-                // This current element is valid as selected HTML for the editor:
-                return intialElement;
-            }
-            else
-            {
-                if (Array.IndexOf(this.CheckParentSelectionElementClassNames(), intialElement.GetType().Name) >= 0)
-                {
-                    IHTMLElement parent = intialElement.parentElement;
-                    return this.getFirstValidSelectionElement(parent);
-                }
-                else
-                {
-                    return null;
-                }
-            }           
-        }
-
-
-        string[] validSelectionElementClassNames()
-        {
-            return new string[] { 
-                "HTMLHeaderElementClass", "HTMLLIElementClass", "HTMLListElementClass", 
-                "HTMLOListElementClass", "HTMLParaElementClass", 
-                "HTMLSpanElementClass", "HTMLTableCaptionClass", "HTMLTableCellClass", 
-                "HTMLTableRowClass" };
-        }
-
-
-        string[] CheckParentSelectionElementClassNames()
-        {
-            return new string[] {
-                "HTMLAnchorElementClass",
-                "HTMLBaseFontElementClass", "HTMLFontElementClass", "HTMLLinkElementClass", 
-                "HTMLPhraseElementClass" };
         }
     }
 }
