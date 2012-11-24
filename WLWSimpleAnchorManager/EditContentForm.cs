449908bf495f7a9518fc7c23d2a482063e45eedb
John Atten <xivsolutions@gmail.com>
2012-11-24 17:00:07 -0600

    EditContentForm - Completed core design and function - Consolidated code in main .cs file.

--- WLWSimpleAnchorManager/EditContentForm.cs
+++ WLWSimpleAnchorManager/EditContentForm.cs
@@ -23,10 +23,18 @@ namespace WLWSimpleAnchorManager
         private Label _currentTabLabel;
 
 
-        public EditContentForm()
+        private AnchorData _currentAnchorSettings;
+        private pnlAnchorEditorBase _currentEditorPanel;
+
+        private string[] _anchorNames;
+
+
+        protected EditContentForm()
         {
             InitializeComponent();
 
+            this.btnOK.Click += new EventHandler(btnOK_Click);
+            this.btnCancel.Click += new EventHandler(btnCancel_Click);
 
             this.tbInsertAnchor.Click += new EventHandler(TabLabel_Click);
             this.tbLinkToAnchor.Click += new EventHandler(TabLabel_Click);
@@ -37,10 +45,144 @@ namespace WLWSimpleAnchorManager
             this.tbInsertAnchor.MouseLeave += new EventHandler(TabLabel_MouseLEave);
             this.tbLinkToAnchor.MouseLeave += new EventHandler(TabLabel_MouseLEave);
 
+            this.tbInsertAnchor.Tag = AnchorTypes.Anchor;
+            this.tbLinkToAnchor.Tag = AnchorTypes.Link;
+
             this._TabLabelGroup = new List<Label>();
             this._TabLabelGroup.Add(tbInsertAnchor);
             this._TabLabelGroup.Add(tbLinkToAnchor);
         }
+
+        void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+
+        public EditContentForm(AnchorData settings, string[] anchorNames) : this()
+        {
+            _anchorNames = anchorNames;
+            _currentAnchorSettings = settings;
+            this.ConfigureForm(_currentAnchorSettings);
+
+            this.btnOK.Enabled = false;
+        }
+
+
+        private void ConfigureForm(AnchorData settings)
+        {
+            if (settings.AnchorType == AnchorTypes.Anchor)
+            {
+                this.FormSetupEditAnchorConfig();
+            }
+
+            if (settings.AnchorType == AnchorTypes.Link)
+            {
+                this.FormSetupEditLinkConfig();
+            }
+        }
+
+
+
+        private void FormSetupEditAnchorConfig()
+        {
+            this.tbLinkToAnchor.Visible = false;
+            this.lblChooseAction.Visible = false;
+            this.tbInsertAnchor.Left = this.lblChooseAction.Left;
+
+            this.SetTabLabelText(tbInsertAnchor, "Edit Existing Anchor");
+
+            this.SelectTabLabel(tbInsertAnchor);
+        }
+
+
+        private void FormSetupEditLinkConfig()
+        {
+            this.tbInsertAnchor.Visible = false;
+            this.lblChooseAction.Visible = false;
+            this.tbLinkToAnchor.Left = this.lblChooseAction.Left;
+
+            this.SetTabLabelText(tbLinkToAnchor, "Edit Link to Anchor");
+
+            this.SelectTabLabel(tbLinkToAnchor);
+        }
+
+
+        private void SetTabLabelText(Label tabLabel, string newText)
+        {
+            int origHeight = tabLabel.Height;
+            tabLabel.AutoSize = true;
+            tabLabel.Text = newText;
+            tabLabel.AutoSize = false;
+            tabLabel.Height = origHeight;
+        }
+
+
+        private void SelectTabLabel(Label selectedTabLabel)
+        {
+            this.TabLabel_Click(selectedTabLabel, new EventArgs());
+        }
+
+
+
+        void btnOK_Click(object sender, EventArgs e)
+        {
+            this.OnSaveAnchor();
+        }
+
+
+        private void OnSaveAnchor()
+        {
+            _currentEditorPanel.PerformSave();
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+
+        void ConfigurationSelectionChanged(AnchorTypes selectedConfiguration)
+        {
+            if (_currentEditorPanel != null)
+            {
+                this.ControlContainer.Controls.Clear();
+            }
+
+            switch (selectedConfiguration)
+            {
+                case AnchorTypes.Anchor:
+                    _currentEditorPanel = new pnlAnchorEditor(_currentAnchorSettings);
+                    break;
+                case AnchorTypes.Link:
+                    _currentEditorPanel = new pnlLinkEditor(_anchorNames, _currentAnchorSettings);
+                    break;
+                case AnchorTypes.None:
+                    _currentEditorPanel = null;
+                    break;
+                default:
+                    _currentEditorPanel = null;
+                    break;
+            }
+
+            _currentEditorPanel.ValidContentDetected += new pnlAnchorEditorBase.ValidAnchorContentHandler(CurrentEditorPanel_ValidContentDetected);
+            _currentEditorPanel.InvalidContentDetected += new pnlAnchorEditorBase.ValidAnchorContentHandler(CurrentEditorPanel_InvalidContentDetected);
+
+            _currentEditorPanel.Dock = DockStyle.Fill;
+            this.ControlContainer.Controls.Add(_currentEditorPanel);
+            _currentEditorPanel.Show();
+
+        }
+
+
+        void CurrentEditorPanel_ValidContentDetected(object sender, EventArgs e)
+        {
+            this.btnOK.Enabled = true;
+        }
+
+
+        void CurrentEditorPanel_InvalidContentDetected(object sender, EventArgs e)
+        {
+            this.btnOK.Enabled = false;
+        }
+
+
 
         void TabLabel_MouseLEave(object sender, EventArgs e)
         {
@@ -78,6 +220,10 @@ namespace WLWSimpleAnchorManager
                     tabLabel.BackColor = INACTIVE_TAB_BACKCOLOR;
                 }
             }
+
+            AnchorTypes newAnchorType = (AnchorTypes)_currentTabLabel.Tag;
+
+            this.ConfigurationSelectionChanged(newAnchorType);
         }
 
         void OnTabLabelEnter(Label selectedTabLabel)
@@ -97,12 +243,6 @@ namespace WLWSimpleAnchorManager
                 selectedTabLabel.BackColor = INACTIVE_TAB_BACKCOLOR;
             }
         }
-
-        private void HorizontalDivider_Click(object sender, EventArgs e)
-        {
-
-        }
-
 
 
 
@@ -135,16 +275,17 @@ namespace WLWSimpleAnchorManager
             this.ControlContainer = new System.Windows.Forms.Panel();
             this.tbInsertAnchor = new System.Windows.Forms.Label();
             this.tbLinkToAnchor = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblChooseAction = new System.Windows.Forms.Label();
             this.HorizontalDivider = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ControlContainer
             // 
-            this.ControlContainer.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.ControlContainer.Location = new System.Drawing.Point(0, 68);
+            this.ControlContainer.Location = new System.Drawing.Point(0, 106);
             this.ControlContainer.Name = "ControlContainer";
-            this.ControlContainer.Size = new System.Drawing.Size(653, 295);
+            this.ControlContainer.Size = new System.Drawing.Size(653, 269);
             this.ControlContainer.TabIndex = 3;
             // 
             // tbInsertAnchor
@@ -152,7 +293,7 @@ namespace WLWSimpleAnchorManager
             this.tbInsertAnchor.BackColor = System.Drawing.Color.Transparent;
             this.tbInsertAnchor.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbInsertAnchor.ForeColor = System.Drawing.Color.LightSteelBlue;
-            this.tbInsertAnchor.Location = new System.Drawing.Point(293, 30);
+            this.tbInsertAnchor.Location = new System.Drawing.Point(306, 67);
             this.tbInsertAnchor.Name = "tbInsertAnchor";
             this.tbInsertAnchor.Size = new System.Drawing.Size(156, 26);
             this.tbInsertAnchor.TabIndex = 4;
@@ -164,44 +305,65 @@ namespace WLWSimpleAnchorManager
             this.tbLinkToAnchor.BackColor = System.Drawing.Color.Transparent;
             this.tbLinkToAnchor.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbLinkToAnchor.ForeColor = System.Drawing.Color.LightSteelBlue;
-            this.tbLinkToAnchor.Location = new System.Drawing.Point(455, 31);
+            this.tbLinkToAnchor.Location = new System.Drawing.Point(468, 69);
             this.tbLinkToAnchor.Name = "tbLinkToAnchor";
-            this.tbLinkToAnchor.Size = new System.Drawing.Size(186, 24);
+            this.tbLinkToAnchor.Size = new System.Drawing.Size(173, 24);
             this.tbLinkToAnchor.TabIndex = 5;
-            this.tbLinkToAnchor.Text = "Link to an Existing Anchor";
+            this.tbLinkToAnchor.Text = "Link to Existing Anchor";
             this.tbLinkToAnchor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label1
+            // lblChooseAction
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
-            this.label1.Location = new System.Drawing.Point(12, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(178, 18);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "What do you want to do?";
+            this.lblChooseAction.AutoSize = true;
+            this.lblChooseAction.BackColor = System.Drawing.Color.Transparent;
+            this.lblChooseAction.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChooseAction.ForeColor = System.Drawing.Color.DarkBlue;
+            this.lblChooseAction.Location = new System.Drawing.Point(12, 72);
+            this.lblChooseAction.Name = "lblChooseAction";
+            this.lblChooseAction.Size = new System.Drawing.Size(178, 18);
+            this.lblChooseAction.TabIndex = 6;
+            this.lblChooseAction.Text = "What do you want to do?";
             // 
             // HorizontalDivider
             // 
             this.HorizontalDivider.BackColor = System.Drawing.Color.DarkBlue;
             this.HorizontalDivider.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.HorizontalDivider.ForeColor = System.Drawing.Color.DarkBlue;
-            this.HorizontalDivider.Location = new System.Drawing.Point(-2, 55);
+            this.HorizontalDivider.Location = new System.Drawing.Point(-2, 93);
             this.HorizontalDivider.Name = "HorizontalDivider";
             this.HorizontalDivider.Size = new System.Drawing.Size(655, 10);
             this.HorizontalDivider.TabIndex = 7;
             this.HorizontalDivider.Text = "What do you want to do?";
-            this.HorizontalDivider.Click += new System.EventHandler(this.HorizontalDivider_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnCancel.Location = new System.Drawing.Point(566, 385);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnOK.Location = new System.Drawing.Point(485, 385);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 8;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
             // 
             // EditContentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(653, 363);
+            this.ClientSize = new System.Drawing.Size(653, 420);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
             this.Controls.Add(this.HorizontalDivider);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblChooseAction);
             this.Controls.Add(this.tbLinkToAnchor);
             this.Controls.Add(this.tbInsertAnchor);
             this.Controls.Add(this.ControlContainer);
@@ -217,9 +379,10 @@ namespace WLWSimpleAnchorManager
         private System.Windows.Forms.Panel ControlContainer;
         private System.Windows.Forms.Label tbInsertAnchor;
         private System.Windows.Forms.Label tbLinkToAnchor;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblChooseAction;
         private System.Windows.Forms.Label HorizontalDivider;
-
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
 
     }
 }
