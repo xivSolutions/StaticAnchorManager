7b677849eb897e42a78b24175d82f088001b01c9
John Atten <xivsolutions@gmail.com>
2012-12-01 17:56:12 -0600

    Added SelectedEditorContent Class

---
+++ WLWSimpleAnchorManager/SelectedEditorContent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WLWSimpleAnchorManager
+{
+    public class SelectedEditorContent
+    {
+        private string _selectedHtml = "";
+        private string _innerHtml = "";
+        private string _selectedText = "";
+
+        public SelectedEditorContent(string selectedHtml, string innerHtml, string selectedText)
+        {
+            _selectedHtml = selectedHtml;
+            _innerHtml = InnerHtml;
+            _selectedText = selectedText;
+        }
+
+        public string SelectedHtml { get { return _selectedHtml; } }
+        public string InnerHtml { get { return _innerHtml; } }
+        public string SelectedText { get { return _selectedText; } }
+    }
+}
